adecb029c359af03f8b928dd582ecaee7c5006f3
lilemman <emmanuelatuahene16@gmail.com>
2023-07-18 18:26:37 -0400

    folders created

---
+++ netflix-capstone/src/utils/Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6ls9r4NpC1c-QohBzHrHWCt8t1m-GrUk",
+  authDomain: "netflix-capstone.firebaseapp.com",
+  projectId: "netflix-capstone",
+  storageBucket: "netflix-capstone.appspot.com",
+  messagingSenderId: "1006679768752",
+  appId: "1:1006679768752:web:0ea5fb8492de50a69cc575",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
